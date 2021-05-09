455f18ba5e59a28904b52b7baa12dd19fbb65307
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-05-09 22:05:01 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
+import { render } from "@testing-library/react";
 
 function App() {
+  // javascript me html injection...... to use js again
+  //  inside html.. interpolation
+  const [value, setValue] = useState("hii");
+  const values = ["world", "hii", "am", "here", "hello"];
+  const titles = ["hello world", "hii", "whos", "this"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* for val in values */}
+      {
+      values.map((val) => (
+        <MyComponent title={val} value={val} />
+      ))
+      }
+    </>
   );
 }
+
+function MyComponent({ value, title }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      {/** Interpolation**/}
+      <button onClick={() => null}>Increment value</button>
+      {value}
+      <h2>Hello second</h2>
+    </>
+  );
+}
+
+// function App() {
+//     // function getData(){
+//     //     //  axios.get("https://example.com:8000/home").then((req,res) => console.log(req);).catch();
+//     // }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
